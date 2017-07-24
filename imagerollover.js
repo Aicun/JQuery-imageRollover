6523e7158869d12d0a2e6c45c11f2ebcfaf4799f
Aicun <luaicun@gmail.com>
2017-07-24 13:44:19 -0400

    image rollover

---
+++ imagerollover.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+    $("#image_rollovers img").each(function(){
+        var oldURL = $(this).attr("src");
+        var newURL = $(this).attr("id");
+
+        var rolloverImage = new Image();
+        rolloverImage.src = newURL;
+
+        /* $(this).hover(
+            function(){
+                $(this).attr("src",newURL);
+            },
+            function(){
+                $(this).attr("src",oldURL);
+            }
+        ); */
+
+        $(this).mouseenter(function(){
+            $(this).attr("src",newURL);
+        });
+
+        $(this).mouseout(function(){
+            $(this).attr("src",oldURL);
+        });
+    });
+});//end ready
